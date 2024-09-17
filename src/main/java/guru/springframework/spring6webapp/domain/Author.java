15628ac336290cc3d5e03150ef9c80084d940d37
vi2998 <58190072+vi2998@users.noreply.github.com>
2024-09-17 18:54:56 +0200

    aggiunta dipendenze

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
 
 import java.util.Set;
 
