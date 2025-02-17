e721e2a19be6061cffa320d7c34d37d536feab86
vi2998 <58190072+vi2998@users.noreply.github.com>
2025-02-17 21:18:55 +0100

    lezione 25

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
